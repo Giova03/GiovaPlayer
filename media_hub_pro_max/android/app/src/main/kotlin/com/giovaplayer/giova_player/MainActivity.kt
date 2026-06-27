dcdf0570cbc2c686c699d01af32dad727c87e6fa
Giova03 <giobamos03@gmail.com>
2026-06-27 10:46:17 +0000

    fix: change MainActivity from FlutterActivity to FlutterFragmentActivity
    audio_service requires FlutterFragmentActivity to work properly.
    This fixes the PlatformException: 'The Activity class declared in
    your AndroidManifest.xml is wrong or has not provided the correct
    FlutterEngine.'

--- media_hub_pro_max/android/app/src/main/kotlin/com/giovaplayer/giova_player/MainActivity.kt
+++ media_hub_pro_max/android/app/src/main/kotlin/com/giovaplayer/giova_player/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.giovaplayer.giova_player
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
